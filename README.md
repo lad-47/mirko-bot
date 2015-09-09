4a36fe9cf670b8a0bea1c52254463e627138529b
lad47@duke.edu <lad47@duke.edu>
2015-09-08 20:31:30 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#MirkoBot
+
+This Twitter bot uses a Markov chain to generate tweets from my
+ friend's group chat messages. It tweets every 5 minutes.
---
+++ mbot.js
@@ -0,0 +1,72 @@
+// Our Twitter library
+var Twit = require('twit');
+
+// We need to include our configuration file
+var T = new Twit(require('./config.js'));
+
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+var group = "Who stole Molly? If any of you need any of the following, please see me: Slim Fast, Bear's Breath Ketchup for the Bold (you need that), Sugar of any kind, Water Enhancers, Molly, Mary Jane, Cookies with mucho fiber, Craisins, Almonds, League of Legends Advice, Doge Adoration Tactics, Song suggestions, The only clothing you need tomorrow is the Robe of Enthusiasm,  the Cloak of Courage, and the Mask of Pride. My father felt that dumping H2O on my head would be the correct form of an alarm today. Does anybody else need to know where my hotel is? So this is my friend. He is in medicine, and these are some bizarre cases he has seen.  Cape Flattery, Cape Flatter-Me, Sing me a tune Of the Sixteenth Sea. My time of the night,  Circlular lions and fleas, Slam Crackle Pop, Cape Flatter-Me. Knock me off my Onager's tee. Mucho Comprendo. Compadres alight. Fuel the fire with Cape Flatter-Me.  Cristian,.. Do we have to do anything crazy tomorrow? This guy makes me so proud. Hi Mr. Lucas. Also, finally, this is Lord Doge, not actually Kathy. #TYBG  ;) If you weren't 420, did you even read it.  You have clearly descended from Sun World. In this world they constantly pedal bikes to keep the sun burning. The sun is expanding and losing heat because the Sun Bikers are getting ever so tired. Sun World has its own version: 50 Shades of Roarngesllow: A Combination of Red, Orange, and Yellow. In this book, everyone has a personality, and the book is extremely spicy and filled with spice. Of course, the book even has the best kind of spice: The Queen Of All Spices. You. Also in Sun World, people continue to steal the sun so the Night Creatures can come down to Earth and steal all the babies and turn them into Night Baby Creatures so thwy can grow up and steal the world from the normal people. There is only one person who can pentakill all the Sun Stealers and Night Creatures. That isn't you, I'm sorry forgive me, it is actually Liam Neeson. He is \"estoy aqui!!!\". No one ever asks Mr. Liam Neeson \"donde estas?\" I just sent that one to the wrong group though. If I said something like \"I love my bae tank prophet\", that would be cause for concern. Everyone will ignore that last message, wrong group. I think independence is a big turn on. If he wants you to be 100% submissive he's got another thing coming! Plus... come on, who compares to Mr. Wow? It means free toys free toys because i just got an owl and named him Mr. Owl. And Cristian... As we reflect on this summer... \"What time are we leaving for church mom?\" \"At exactly 4:20\" Mirko's thought process: YOOOOOOOOOOOOOOOOO. Dirigibles. YOU KNOW IM BACK HOME WHEN!!!!!!!! Ingram just said \"ggwp\" to UNC. GOON Squad president approves L D Train as the saxophone's next section leader. Let the reign begin. ilybae. nah ive been shredding parker so its in my room. matt should grab me one too. my cover is basically saying gg to all other candidates. HANNAH WE SAW YOUR HAIR!!!!!!!!! #yourmostroyalmajesty its fine leave it be next year though they better be ready to go to work ... and you know we outwork and outskill, cuz we got the jeff au swagger #saxlinebestline #GOONsquad trombones need a jeff au tuning . you should introduce yourself as The Queen. My cowboy hat is at home :( It'd have to be over the summer.... But a shirtless cowboy WOULD ... make a mark, on President Brodhead. So what if you wanted to go shirtless with a cowboy hat, boots, and jeans? Because if a man showed up like that at my fromt door....  so justice winslow is out with this short blonde at bull city nbd. the prez had some important goon squad business-related matters to take care of . I LIKE IT!!! Nova guards look like they're on Novacane because they're so sleepy. Just gunna leave this up here... Hugh come at me ;) Shannon you know he's Australian right? And the Thunder From Down Under strippers are Australian? Conclusion: I love Australian men... I mean, I'm having a night where I stare at Hugh Jackman and talk about his abs... >.< In afinancially frugal way.  I wish I never had to leave these Brazilians.......................... When you get re-routed by someone that speaks english 50% good, shit happens.. shouldnt ask for it if youre in miami goin to JFK BO$$. He shall reveal himself. In Time. #kingsofthefield #confirmed IF IM LUCKY MR TALENT WILL RUB HIS TENTACLES ON ME!!!!!!!!!!!!!! #GREENLIGHT #YELLOWWIZARD illuminati and snoop. welcome to spring break my friends ;) Isaac, if you listen to \"Steady Mobbin\" by Young Money, or \"Black Skinhead\" by Kanye West, you will gain much insight into my spring break ;) I might make it, if I do ... you'll know... Babies - GOON squad President (Uncle Me) is running for VP. He would love to be able to count on all of your votes and support but if he needs to earn it, he would be happy to talk to you personally about the role and why he'd be an excellent VP. GOON Squad born-again-president filled it out ages ago. I HAVE TOO MUCH SUGAR IN MY METABOLISM!!!!!!!!! ICE BREAKERS DO MORE THAN BREAK THE ICE!!!!!!!!!!!!!!!!!! ANDREW!!!!!!!!!!!!!!!!!!! IM HEDWIG!!!!!!!!!!!!!!! i love sugar!!!!!!!!!!!!!! MR FRESh!!!! The Heaven of Hell. Shackled by DUMB's Cleaning Squad. Bells of Anarchy. Ive had a lot of chocolate recently ;) Loving this Slim Fast #chocolatefantasy I can tell you: 1000 staxxx for saxes, no stacks for rest of band. My back hurts cuz ive had to carry their backsides so hard. GG Band, saxes r going to UNC ninjaz!!! mom i tried but hes too quick!!! he left on this note: \"excuse me, yo door locked\" me: \"yeah that door's always locked, just use the one next to it\" my thoughts: \"dude there are two doors back to back if one's locked and this is the only way in and out of the library...\" SQUIRREL(S)!!! mom rest. stay. if u need anything u holler at yo sun! because i brought it to him a couple days ago from good ole MP, where you always get fresh fruit. Just jidding. hes in the banana suit. yeah u do. theyre called carrots where im from. basic round 2. its funny because u dont like selfies ;) this changes my whole perception of you. just kidding. #pumpkinmaster this changes everything. first of all its Mirky. #grandmasters i still cant pronounce that last name correctly. THE QUEEN IS BAAAAAAAAAAAAACK!!!!!!!!!!!!!!! all i know is that its a motorcyle that i rode so hard. is that his motorcycle? #1000staxtoheaven And then there's this... Wrong P... GG shirt. i open up the groupme to mr distell the fangirl... thats how we roll #finnaflip God save the Queen. To The Saxes and GOON Squad: this semester????????? the president is berry tired. queen duties we understand. to isaac: Yeah you are!!!! gotta say it may have been a loss but nothing to be ashamed of considering ASU is 15th in the country and can give really good teams a run for their money. THATS RIGHT!!! BOOM!!!!! 20-17 UH HUH UH HUH OH YEAH BOOM-YEAH!!!!!!! I can hear you guys louder than the ASU band on the shorts. I heard u guys on Power. The president ... got his phone screen fixed #hellyeag  every day is an odyssey day. She says Thank You and you're very beautiful. getting a manicure and pedicure #loveit is there any other animal that needs a pic of his or her own? lets just say i have a VIP only wild side. it was, meant as like \"wow i have a lot of hair\" plus i have a gf soooooo Thank you Queen, the president loves you all. Me: \"Are you at the stop where the buses pick u up?\" Evan: \"Thats called the bus stop\" Me: \"Shu U\" Hey guys RTP is 4 or 4:30? I thought it was 4:30 but Jeff sent an email that said at 4 we do something so I just wanna make sure. my face when momma and i send the same pic... So no worries about this bone tryna spoon us. mainly picardio: he's the hades to my zeus. no wombo combo yet . lololol alliance OP, GG. well not yet i just feel as president of the GOON Squad that I need to keep my goons, mom, queen, and sexy saxes safe from impending spooning. no they're each assigned one victim. They can't get assigned someone in their section (so like they can't assign me to kill any one of u). people have been resurrected to solely kill people u gotta watch out. there r hell hounds now. guys watch out. uhh goon squad, not good. otherwise our acronyms wouldnt make sense. we actually need FSU to win this is so awkward... THATS US GUYZ!!!!!!!!!!!!!! we need to celebrate. oh i still love u guys. i love u guys. you want us to poke out his eye??? ok that \"beats\" the wolverine idea. im gunna snare him in a not very gentleman-like way. Aww really? Because I was going for something in the wolverine-spoon family. i heart u still. teach them the airplane.  I love you mom. You're welcome. In other news, who's going to the dance thing? I might not be able to go and I need someone to tell them about the airplane... the flutes are trying to \"blow\" it off as nothing (see the pun? blow into the flute, blow it off?). so how do we skew it? from now in i'm a scout. hannah the president and queen and mother have been targeted, hellooooo??????? but now that i'm dead: \"i'm dead, but can we spoon anyways?\" 10/10 would spoon again. MOM NO!!!!!!!!!!!!!!! EVERYONE KISSES FOR MOM!!!!!! forming a vengance squad he's tryna eliminate the saxes. im sorry GOON Squad I failed us all, the president has been spooned. how do u guys get spooned so hard? I was just at MP for a whole hour and no one even flinched. Great job at the Revolution Chapter Meeting today. For those who were not there please fill in the details with someone. Our goal is to have a fully realized Revolution by the time we hit UNC, so when they think they know the atmosphere they have NO FREaKING IDEA what's coming. We must take the necessary steps to ensure maximum Revolution power.  We are all Children of the Night. We must choose how to use it. --- Snoozles I love you all. The shortest war occured in 1896 when Zanzibar sureendered to Britain in 38 minutes. Ladies, gentlemen, and some anacondas... let's make quick work of Armstrong ... TODAY!!!! Hunting has been completed for tonight... congrats to the Goon Squad, and I feel we need an Elite Hunting Club but that's the Goon Squad so good job. Also, for fellow vampires and blackthorn raven queens, Feeding Night is Wed. so be on your toes for that. ALSO, for those of you going to thebball game, get pumped for hugs and chocolate and bball and your very sexy selves. oh boy am i confused but everyone on the saxes can read this?";
+
+//fs.readFile('group.txt', function(err, data) {
+	//if (err) throw err;
+	//console.log(data);
+	//var words = data.split(/\s+/);
+//});
+
+var words = group.split(' ');
+var wordstats = {};
+
+for (var j=0;j<words.length - 1;j++){
+	if(wordstats.hasOwnProperty(words[j])){
+		wordstats[words[j]].push(words[j+1]);
+	} else {
+		wordstats[words[j]] = [words[j+1]];
+	}
+}
+
+var choice = function(a) {
+	var i = Math.floor(a.length*Math.random());
+	return a[i];
+}
+
+var make_tweet = function(max_length) {
+	word = choice(words);
+	var tweet = [word];
+	while(wordstats.hasOwnProperty(word)) {
+		var next_words = wordstats[word];
+		word = choice(next_words);
+		tweet.push(word);
+		var str = tweet.join(' ');
+		if (str.length > max_length) {
+			tweet.pop();
+			return tweet.join(' ');
+		}
+	}
+	return tweet.join(' ');
+}
+
+
+function tweet() {
+	  var newtweet = make_tweet(90);
+	  T.post('statuses/update', { status: newtweet }, function(err,reply) {
+		  if (err) {
+			  console.log('error:', err);
+		  } else {
+			  console.log('reply:', reply);
+		  }
+	  });
+}
+
+var thing = make_tweet(90);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+console.log(thing);
+// Try to retweet something as soon as we run the program...
+tweet();
+// ...and then every hour after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
+setInterval(tweet, 1000 * 60 * 5);
